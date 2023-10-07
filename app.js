0f73266cad59850417fc268078476f2fa325fdea
Priyaranjan Kumar <priyaranjan.moscow@gmail.com>
2023-10-07 20:28:37 +0530

    manipulation of HTML DOM with React

---
+++ app.js
@@ -0,0 +1,30 @@
+/**
+ *
+ * <div id="parent">
+ *   <div id="child">
+ *      <h1></h1>
+ *   </div>
+ * </div>
+ *
+ * ReactElement(Object)=>HTML(Browser Understands)
+ *
+ */
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am an H1 tag"),
+    React.createElement("h2", {}, "I am in H2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am an H1 tag"),
+    React.createElement("h2", {}, "I am in H2 tag"),
+  ]),
+]);
+
+// const heading = React.createElement("h1", {}, "Hello world from React!");
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
